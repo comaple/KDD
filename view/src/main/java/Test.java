a99fae7654c2a9657abc78dd89a0ea5a89ef2992
zhangshengtao <zhangshengtao@kdd.com>
2014-08-23 12:06:55 +0800

    first commit.

---
+++ view/src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: comaple.zhang
+ * Date: 14-8-23
+ * Time: 上午11:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+            System.out.println("aldfj");
+    }
+
+}
