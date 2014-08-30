98a121ea280a33315e0438e1b9cec82b29026a75
lyb <lybing315@163.com>
2014-08-30 18:36:03 +0800

    tag相关

---
+++ view/src/main/java/com/easyminning/view/web/StepTagController.java
@@ -0,0 +1,49 @@
+package com.easyminning.view.web;
+
+import com.easyminning.tag.StepTagService;
+import com.easyminning.tag.StepTag;
+import com.mongodb.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
+@Controller
+@Scope("prototype")
+public class StepTagController extends BaseController {
+
+    @Autowired
+    private StepTagService stepTagService;
+
+    /**
+     * 传步骤编号，例如1，2，3，4，5，6
+     * @param step
+     */
+    @RequestMapping(value = "/steptag", method = RequestMethod.GET)
+    public void test(String step,Integer size) {
+        if (step == null || "".equals(step.trim())) renderJson(new ArrayList<Map>());
+        if (size == null || size < 0) size = 20;
+
+        QueryBuilder queryBuilder = QueryBuilder.start("step").is(step);
+        List<StepTag> stepTagList = stepTagService.select(queryBuilder, 1, size, StepTag.class);
+        List<Map> result = new ArrayList<Map>();
+        Map<String,String> map1 = new HashMap<String,String>();
+        map1.put("tag", "标签1");
+        map1.put("weight", "10");
+        result.add(map1);
+
+        Map<String,String> map2 = new HashMap<String, String>();
+        map2.put("tag", "标签2");
+        map2.put("weight", "9");
+        result.add(map2);
+        renderJson(result);
+    }
+
+}
---
+++ view/src/main/java/com/easyminning/view/web/TagDocController.java
@@ -0,0 +1,50 @@
+package com.easyminning.view.web;
+
+import com.easyminning.tag.TagDoc;
+import com.easyminning.tag.TagDocService;
+import com.mongodb.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@Scope("prototype")
+public class TagDocController extends BaseController {
+
+    @Autowired
+    private TagDocService tagDocService;
+
+    /**
+     * 传tag名称，例如美国
+     * @param tag
+     */
+    @RequestMapping(value = "/tagdoc", method = RequestMethod.GET)
+    public void test(String tag, Integer size) {
+        if (tag == null || "".equals(tag.trim())) renderJson(new ArrayList<Map>());
+        if (size == null || size < 0) size = 20;
+
+        QueryBuilder queryBuilder = QueryBuilder.start("tagItem1").is(tag);
+        List<TagDoc> tagDocList = tagDocService.select(queryBuilder,1,size,TagDoc.class);
+        List<Map> result = new ArrayList<Map>();
+        Map<String,String> map1 = new HashMap<String,String>();
+        map1.put("title", "标题1");
+        map1.put("abstract", "摘要1");
+        map1.put("body", "正文1");
+        map1.put("weight", "10");
+        result.add(map1);
+
+        Map<String,String> map2 = new HashMap<String, String>();
+        map2.put("abstract", "摘要1");
+        map2.put("body", "正文1");
+        map2.put("weight", "10");
+        renderJson(result);
+    }
+
+}
