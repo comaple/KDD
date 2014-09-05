27f4e46dad1d30c3027544efc3891df78b24c66c
comaple <comaple@126.com>
2014-09-05 21:37:54 +0800

    commit

---
+++ util/src/main/java/com/easyminning/tag/BaseModel.java
@@ -0,0 +1,21 @@
+package com.easyminning.tag;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2014/9/5.
+ */
+public class BaseModel {
+
+    private Map _id;
+
+    public Map get_id() {
+        return _id;
+    }
+
+    public void set_id(Map _id) {
+        this._id = _id;
+    }
+
+
+}
