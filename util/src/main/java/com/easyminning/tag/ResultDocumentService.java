7e0783a84ecdcc866b4345436aaa2d601ee9b467
lyb <lybing315@163.com>
2014-09-06 23:32:55 +0800

    step seed commit

--- util/src/main/java/com/easyminning/tag/ResultDocumentService.java
+++ util/src/main/java/com/easyminning/tag/ResultDocumentService.java
@@ -24,8 +24,6 @@ public class ResultDocumentService extends AbstractService<ResultDocument> {
     }
 
 
-    public SimpleMongoDBClient2<ResultDocument> simpleMongoDBClient2;
-
     public void setCollectionName(String collectionName) {
         this.collectionName = "resultdocument";
     }
