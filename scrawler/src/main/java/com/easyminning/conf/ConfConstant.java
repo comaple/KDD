72c8085a6ce98cc2fa37f50fe40729df5cfa8c46
longyan <longyan_jerry@foxmail.com>
2014-09-08 14:53:30 +0800

    u

--- scrawler/src/main/java/com/easyminning/conf/ConfConstant.java
+++ scrawler/src/main/java/com/easyminning/conf/ConfConstant.java
@@ -7,16 +7,18 @@ public class ConfConstant {
     public static final String ObjectSplit = ";;;"; //配置文件中一个对象的分隔符
     public static final String TemplateSplit = "%%%"; //模板配置中正则和模板文件的分隔符
 
-    public static final String SEEDS = "seeds";
+
     public static final String CRAWLDBPATH = "crawldbpath";
     public static final String DOWNLOADPATH = "downloadpath";
-    public static final String DEPTH = "depth";
     public static final String RESUMABLE = "resumable";
+
+
+    //从这以下的参数都是支持配置文件动态更新的
+    public static final String SEEDS = "seeds";
+    public static final String DEPTH = "depth";
     public static final String THREADS = "threads";
     public static final String TIMESPAN = "timespan";
     public static final String MAXARTICLENUM = "maxarticlenum";
-
-    //从这以下的参数都是支持配置文件动态更新的
     public static final String REPEATABLEREGEX = "repeatableregex";
     public static final String POSITIVEREGEX = "positiveregex";
     public static final String NEGATIVEREGEX = "negativeregex";
