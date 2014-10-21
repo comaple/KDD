a1e094d106d429e5e27d81b235c3131f2a86f6cb
lyb <lybing315@163.com>
2014-10-21 23:42:00 +0800

    step seed commit

--- scrawler/src/main/java/com/easyminning/extractor/TemplateExtractor.java
+++ scrawler/src/main/java/com/easyminning/extractor/TemplateExtractor.java
@@ -4,6 +4,7 @@ import cn.edu.hfut.dmic.webcollector.model.Page;
 import com.easyminning.conf.ConfConstant;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,6 +13,8 @@ import java.util.regex.Pattern;
  */
 public class TemplateExtractor extends Extractor {
     private HashMap<String,String> templateRex;
+
+    private static String SEPERATOR = "%%%";
 
     private final static String [][] filters = {
             {"(?is)<!DOCTYPE.*?>", ""},
@@ -41,34 +44,65 @@ public class TemplateExtractor extends Extractor {
         Article article = new Article();
         article.url = page.url;
         //匹配作者
-        Pattern p = Pattern.compile(templateRex.get(ConfConstant.AUTHOR));
+        String author = templateRex.get(ConfConstant.AUTHOR);
+        int authorGroup = 0;
+        String authorRegx = author;
+        if (author.contains(SEPERATOR)) {
+            authorRegx = author.split(SEPERATOR)[0];
+            authorGroup = Integer.parseInt(author.split(SEPERATOR)[1]);
+        }
+        Pattern p = Pattern.compile(authorRegx);
         Matcher m = p.matcher(html);
         if(m.find()){
-            String authorHtml = m.group();
+            String authorHtml = m.group(authorGroup);
             article.author = authorHtml.replaceAll("<.*?>","").replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
         }
         //发布时间
-        p = Pattern.compile(templateRex.get(ConfConstant.PUBLISHDATE));
+        String publishTime = templateRex.get(ConfConstant.PUBLISHDATE);
+        int publishTimeGroup = 0;
+        String publishTimeRegx = publishTime;
+        if (publishTime.contains(SEPERATOR)) {
+            publishTimeRegx = publishTime.split(SEPERATOR)[0];
+            publishTimeGroup = Integer.parseInt(publishTime.split(SEPERATOR)[1]);
+        }
+        p = Pattern.compile(publishTimeRegx);
         m = p.matcher(html);
         if(m.find()){
-            String dateHtml = m.group();
+            String dateHtml = m.group(publishTimeGroup);
             article.publishDate = dateHtml.replaceAll("<.*?>","").replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
         }
         //标题
-        p = Pattern.compile(templateRex.get(ConfConstant.TITLE));
+        String title = templateRex.get(ConfConstant.TITLE);
+        int titleGroup = 0;
+        String titleRegx = title;
+        if (title.contains(SEPERATOR)) {
+            titleRegx = title.split(SEPERATOR)[0];
+            titleGroup = Integer.parseInt(title.split(SEPERATOR)[1]);
+        }
+        p = Pattern.compile(titleRegx);
         m = p.matcher(html);
         if(m.find()){
-            String titleHtml = m.group();
+            String titleHtml = m.group(titleGroup);
             article.title = titleHtml.replaceAll("<.*?>","").replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
         }
         //正文
-        p = Pattern.compile(templateRex.get(ConfConstant.MAINCONTENT));
+        String content = templateRex.get(ConfConstant.MAINCONTENT);
+        int contentGroup = 0;
+        String contentRegx = content;
+        if (content.contains(SEPERATOR)) {
+            contentRegx = content.split(SEPERATOR)[0];
+            contentGroup = Integer.parseInt(content.split(SEPERATOR)[1]);
+        }
+        p = Pattern.compile(contentRegx);
         m = p.matcher(html);
         if(m.find()){
-            article.contextWithTag = m.group().replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
+            article.contextWithTag = m.group(contentGroup).replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
             //article.context = article.contextWithTag.replaceAll("<.*?>","").trim();
             article.context = article.contextWithTag.replaceAll("<.*?>","").replaceAll("\\s*\n\\s*"," ").replaceAll("\\s*\r\\s*"," ").trim();
         }
         return article;
     }
+
+
+
 }
